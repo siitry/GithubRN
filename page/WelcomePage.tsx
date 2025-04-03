928f09490ffc9e0619829b44e3155c8ff4ab0314
siitry <siitry1949@gmail.com>
2025-04-03 17:54:52 +0800

    初始学习RN，感觉挺爽的

---
+++ page/WelcomePage.tsx
@@ -0,0 +1,16 @@
+import type {FC} from 'react';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export const WelcomePage: FC = () => {
+  return (
+    <View>
+      {/*<Text style={styles}>666</Text>*/}
+      <Text>666</Text>
+    </View>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   container: {},
+// });
